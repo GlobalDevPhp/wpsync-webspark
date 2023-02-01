8e444d6d2d3c98d25842f28605560b68fd10b958
Yevhen <itnotes.org.ua@gmail.com>
2023-02-01 10:42:01 +0200

    init commit

---
+++ readme.txt
@@ -0,0 +1,17 @@
+# wpsync-webspark #
+* Contributors: (this should be a list of wordpress.org userid's)
+* Tags: api, import, woocommerce
+* Tested up to: 5.7.1
+* Stable tag: 1.7.3
+* Requires at least: 3.1
+* Requires PHP: 7.4
+* License: GPLv2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+## Description ##
+Test task for the position WordPress developer - synchronization of the DB of products with the rest
+
+## Changelog ##
+
+### 1.0.0 ###
+
---
+++ wpsync-webspark.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: wpsync-webspark
+ * Plugin URI: http://woocommerce.com/products/wpsync-webspark/
+ * Description: Test task for the position WordPress developer - synchronization of the DB of products with the rest
+ * Version: 1.0.0
+ * Author: Khromykh E.
+ * Author URI: https://webspark.dev/
+ * Developer: Khromykh E.
+ * Developer URI: https://webspark.dev/
+ * Text Domain: woocommerce-extension
+ * Domain Path: /languages
+ *
+ * Woo: 12345:342928dfsfhsf8429842374wdf4234sfd
+ * WC requires at least: 7.3.0
+ * WC tested up to: 7.3.0
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
